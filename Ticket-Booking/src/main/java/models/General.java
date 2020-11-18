b4011d6e42a5196e82841b2fa3e807e512ee288f
HimadriBanerjee <himadribanerjee98@gmail.com>
2020-11-18 20:39:54 +0530

    third commit

---
+++ Ticket-Booking/src/main/java/models/General.java
@@ -0,0 +1,22 @@
+package models;
+import enums.PassengerType;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class General extends Passenger {
+    public General(String name, int age, String address, int idCard, Long contact) {
+        super(name, age, address, idCard, contact, PassengerType.GENERAL);
+    }
+}
+       /* public General(String name,int age, String address,int idCard,long contact ){
+            this.setName(name);
+            this.setAge(age);
+            this.setContact(contact);
+            this.setAddress(address);
+            this.setIdCard(idCard);
+        }
+        @Override
+        public enums.PassengerType getPassengerType() {return  PassengerType.GENERAL;}
+    }*/
+
