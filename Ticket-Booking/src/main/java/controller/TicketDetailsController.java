12ce7d44403b609bd011a8baa72eea6e0486e417
HimadriBanerjee <himadribanerjee98@gmail.com>
2020-11-20 15:10:26 +0530

    hi

--- Ticket-Booking/src/main/java/controller/TicketDetailsController.java
+++ Ticket-Booking/src/main/java/controller/TicketDetailsController.java
@@ -1,4 +1,4 @@
-package controller;
+/*package controller;
 import service.TicketDetailsService;
 import service.TrainDetailsService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.awt.*;
+
 import java.util.Map;
 @RestController
 @RequiredArgsConstructor
@@ -17,11 +17,11 @@ public class TicketDetailsController {
     private final TrainDetailsService trainDetailsService;
     @GetMapping(path = "ticket")
     public Map<String, Object> GetTicketAttributes(@RequestParam(value = "ticketID")Integer ticketID){
-        return this.ticketDetailsService.TicketAttributes()(ticketID);
+        return this.ticketDetailsService.TicketAttributes(ticketID);
     }
     @GetMapping(path = "train")
-    public Map<String, Object> GetTrainDetails(@RequestParam(value = "stationID_A")String stationID_A,
-                                               @RequestParam(value = "stationID_D")String stationID_D){
-        return this.trainDetailService.TrainAttributes(stationID_A,stationID_D);
+    public Map<String, Object> GetTrainDetails(@RequestParam(value = "trainID")String trainID
+    ){
+        return this.trainDetailsService.TrainAttributes(trainID);
     }
-}
+}*/
